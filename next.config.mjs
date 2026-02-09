7b13df6f5299cc06cf54068b5bd8e73b7846dae5
Your Name <your_email@example.com>
2026-02-09 18:45:14 -0500

    fix: Disable experimental testmode and align SWC versions

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,7 @@ const __dirname = path.dirname(__filename);
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  turbopack: {},
 
   outputFileTracingRoot: __dirname,
   reactStrictMode: false,
